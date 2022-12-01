732501b9039966aa59182fa296eb8ccca778d349
dabezad <dabezad@CCTL130-02>
2022-12-01 13:31:57 +0100

    fg

---
+++ ModeloDominio/PersonalBiblioteca.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDominio
+{
+    internal class PersonalBiblioteca
+    {
+        private string nombre;
+        private string contraseña;
+
+        public PersonalBiblioteca(string nombre, string contraseña)
+        {
+            this.nombre = nombre;
+            this.contraseña = contraseña;
+        }
+
+        public string Nombre { get { return nombre; } }
+        public string Contraseña { get { return contraseña; } }
+    }
+
+
+
+    internal class PersonalAdquisiciones: PersonalBiblioteca
+    {
+        public PersonalAdquisiciones(string nombre, string contraseña) : base(nombre, contraseña) { }
+    }
+
+
+    internal class PersonalSala : PersonalBiblioteca
+    {
+        public PersonalSala(string nombre, string contraseña) : base(nombre, contraseña) { }
+    }
+}
