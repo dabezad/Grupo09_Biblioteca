8ca713ac0b982f9ebb2f446394ff86308f711121
ungonzal <ungonzal@CCTL130-03>
2022-12-01 13:23:33 +0100

    assas

--- ModeloDominio/Ejemplar.cs
+++ ModeloDominio/Ejemplar.cs
@@ -9,7 +9,17 @@ namespace ModeloDominio
     internal class Ejemplar
     {
         private string codigo;
-        private Estado prestado;
+        private string estado;
+        private Libro libro;
 
+        private Ejemplar(string codigo, string estado, Libro libro)
+        {
+            this.codigo = codigo;
+            this.estado = estado;
+            this.libro = libro;
+        }
+
+        public string Codigo { get { return codigo; } }
+        public string Estado { get { return estado; } set { this.estado = value; } }
     }
 }
--- ModeloDominio/Prestamo.cs
+++ ModeloDominio/Prestamo.cs
@@ -8,7 +8,26 @@ namespace ModeloDominio
 {
     public class Prestamo
     {
+        private string codigo;
+        private Usuario usuario;
+        private Ejemplar[] ejemplares;
+        private DateTime fRealizado;
+        private DateTime fFinPrestamo;
+        private string estado;
 
+        private Prestamo(string codigo, Usuario usuario, Ejemplar[] ejemplares, DateTime fRealizado, DateTime fFinPrestamo, string estado)
+        {
+            this.codigo = codigo;
+            this.usuario = usuario;
+            this.ejemplares = ejemplares;
+            this.fRealizado = fRealizado;
+            this.fFinPrestamo = fFinPrestamo;
+            this.estado = estado;
+        }
 
+        public string Codigo { get { return this.codigo; } }
+        public DateTime FRealizado { get { return this.fRealizado; } }
+        public DateTime FFinPrestamo { get { return this.fFinPrestamo; } }
+        public string Estado { get { return this.estado; } set { this.estado = value; } }
     }
 }
