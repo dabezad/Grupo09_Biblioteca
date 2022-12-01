709172e6ac257c60e10a4b134cdd49934ad53f19
dabezad <dabezad@CCTL130-02>
2022-12-01 13:54:13 +0100

    fgrt

---
+++ Persistencia/Transformadores.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    internal class Transformadores
+    {
+        public static UsuarioDato UsuarioADato(Usuario u)
+        {
+            UsuarioDato ud = new UsuarioDato(u.Dni, u.Nombre);
+            return ud;
+        }
+    }
+}
---
+++ Persistencia/UsuarioDato.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    internal class UsuarioDato: Entity<string>
+    {
+        private string dni;
+        private string nombre;
+        public UsuarioDato(string dni, string nombre): base(dni)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+        }
+    }
+}
